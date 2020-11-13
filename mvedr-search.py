d903cc015c66b3135f5290fdfbe09488fbfc458a
Carlos Munoz <charly.munoz@gmail.com>
2020-11-13 17:48:36 +0100

    bugs

--- mvedr-search.py
+++ mvedr-search.py
@@ -5,12 +5,12 @@ import os
 import time
 import logging
 import xml.etree.ElementTree as ET
+import argparse
+import glob
 from credenciales import user, password
 
 # Path actual
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-# Fichero con consulta XML
-XMLFILE = 'hunting' + os.sep + 'service_crashed.xml'
 
 # Configure local logger
 # Enable logging, this will also direct built-in DXL log messages.
@@ -79,7 +79,6 @@ class MvEDR():
                 logger.error('[¡] Error transformando la respuesta de la consulta a formato json')
                 raise
         else:
-            print(r.json())
             raise ConnectionError('[¡] Error al realizar la consulta, status code {}'.format(r.status_code))
     
     def query_status(self, id):
@@ -147,14 +146,171 @@ def parse_xml(xml_file):
         query['aggregated']  = json.loads(aggregated)
 
     except Exception as error:
-        logger.error ('[¡] Error parseando el fichero xml de consulta')
-        logger.error(error)
-        sys.exit()
+        raise
 
     return (query_name, query_description, query)
 
-if __name__ == '__main__':
- 
+def parseargs():
+    '''
+    Description: Function in charge of the CLI parameters
+    Input:       No input
+    Output:      Parsed arguments
+    '''
+    description = 'Set parameters for McAfee Mvision EDR search'
+    prog = 'mvedr-search.py'
+    usage = '\nmvedr-search.py -u Username -p Password -i file_name or folder [-o output format]'
+    epilog = 'Carlos Munoz (carlos.munozgarrido@mcafee.com)\n%(prog)s 1.0 (13/11/2020)'
+
+    parser = argparse.ArgumentParser(epilog=epilog, usage=usage, prog=prog, description=description, formatter_class=argparse.RawTextHelpFormatter)
+
+    auth_group = parser.add_argument_group("Required parameters")
+
+    
+    arg_help = "Username for Mvision EDR platform"
+    auth_group.add_argument('-u', required=True, action='store', dest='username', help=arg_help, metavar="")
+
+    arg_help = "Password for Mvision EDR platform"
+    auth_group.add_argument('-p', required=True, action='store', dest='password', help=arg_help, metavar="")
+
+    arg_help = "Path to file or folder"
+    auth_group.add_argument('-i', required=True, default="", action='store', dest='input', help=arg_help,
+                        metavar="")
+
+    arg_help = "Output format"
+    parser.add_argument('-o', required=False, default="json", action='store', dest='output', choices=['json', 'csv', 'pdf'], 
+                        help=arg_help, metavar="")
+
+    parser.add_argument('--version', action='version', version='Carlos Munoz (carlos.munozgarrido@mcafee.com)\n%(prog)s 1.0 (13/11/2020)')
+
+    return parser.parse_args()
+
+def is_folder(input_value):
+        # Check if input_value is folder or file
+        if os.path.isdir(input_value):
+            is_folder = True
+        else:
+            is_folder = False
+
+        return is_folder
+
+def check_query_status(id, mvedr):
+    error_count = 0
+    while True:        
+        try:
+            query_value = mvedr.query_status(id)
+            status = query_value['status']
+            if status == "FINISHED" or error_count == 3:
+                break
+            else:
+                logger.info("[+] Proccesing query")
+                logger.info(query_value)
+                time.sleep(10)
+        except Exception as error:
+            error_count += 1
+
+    if error_count == 3:
+        raise ValueError('[!] Too many detected errors when checking status of the query')
+
+    return query_value
+
+
+def main():
+    option = parseargs()
+
+    DC = "EU"
+    username = option.username
+    password = option.password
+
+    input_value = option.input 
+
+    
+    if input_value.endswith('\\') or input_value.endswith('/'):
+        input_value = input_value[:-1]
+
+    output_value = option.output
+
+    if not os.path.exists(input_value):
+        logger.error("[¡] input value (file or folder) doesn't exists")
+        sys.exit()
+
+    try:
+        mvedr = MvEDR(DC, username, password)
+    except ValueError as error:
+        logger.error(error)
+        sys.exit()
+    except Exception as error:
+        logger.error(error)
+        sys.exit()
+
+    logger.info('[+] Autenticación realizada')
+
+
+    if is_folder(input_value):
+        xml_files = glob.glob(input_value + os.sep + '*.xml')
+    else:
+        xml_files = [input_value]
+    
+    for xml_file in xml_files:
+        logger.info('[+] Processing file query {}'.format(xml_file))
+
+        # Parsing XML file if Exception continue with next file in folder
+        try:
+            (query_name, query_description, query) = parse_xml(xml_file)
+        except Exception as error:
+            logger.error ('[¡] Error parsing query file, check format')
+            continue
+            
+        # Sending query to Mvision EDR if Exception continue with next file in folder
+        logger.info('[+] Sending query {} to Mvision EDR'.format(query_name))
+        logger.info('[+] Query Description: \n\t{}'.format(query_description))
+
+        try:
+            query_request = mvedr.query(query)
+        except Exception as error:
+            logger.error('[!] Error sending query to Mvision EDR')
+            continue
+
+        # Obtaining query Identificator if Exception continue with next file in folder
+        try:
+            id = query_request['id']
+        except Exception as error:
+            logger.error('[¡] Error obtaining the identificator of the query')
+            continue
+
+        # Checking status of the query if Exceptioncontinue with next file in folder
+        try:
+            query_value = check_query_status(id, mvedr)
+        except Exception as error:
+            logger.error(error)
+            continue
+
+        logger.info('[+] Query finish')
+    
+        logger.info('[+] Getting information')
+
+        # Getting the total number of returned lines
+        total_lines = query_value['results']
+
+        # Code to go through the returned content
+        page = 25 # Show 25 lines per page
+        for i in range(0, int(total_lines), page):
+            offset = i
+            limit  = offset + page
+            try:
+                results = mvedr.getQueryData(id, offset=i, limit=limit)
+            except Exception as error:
+                logger.error(error)
+                break
+
+            logger.info("[+] Query result - values from {} to {}".format(offset, limit))
+            logger.info(results)
+
+        logger.info('[+] End process for query')
+
+
+
+def backup():
+    XMLFILE = 'hunting\\users\\admin_connection.xml'
     xmlfile_path = CURRENT_DIR + os.sep + XMLFILE
     (query_name, query_description, query) = parse_xml(xmlfile_path)
 
@@ -231,3 +387,5 @@ if __name__ == '__main__':
 
 
 
+if __name__ == '__main__':
+    main()
